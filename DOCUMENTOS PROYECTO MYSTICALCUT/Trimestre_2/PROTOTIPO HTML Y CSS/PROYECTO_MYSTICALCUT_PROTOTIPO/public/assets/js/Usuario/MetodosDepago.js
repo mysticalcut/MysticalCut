fbdd6a506bb536458d52578b09421bbf7d6d9daa
Andres1Leon <oaleon06@soy.sena.edu.co>
2024-11-28 16:03:14 -0500

    se elimina carpeta repetida

--- DOCUMENTOS PROYECTO MYSTICALCUT/Trimestre_2/PROTOTIPO HTML Y CSS/PROYECTO_MYSTICALCUT_PROTOTIPO/public/assets/js/Usuario/MetodosDepago.js
+++
@@ -1,7 +0,0 @@
-function mostrarCamposTarjeta() {
-    document.getElementById('campos-tarjeta').style.display = 'block';
-}
-
-function ocultarCamposTarjeta() {
-    document.getElementById('campos-tarjeta').style.display = 'none';
-}
