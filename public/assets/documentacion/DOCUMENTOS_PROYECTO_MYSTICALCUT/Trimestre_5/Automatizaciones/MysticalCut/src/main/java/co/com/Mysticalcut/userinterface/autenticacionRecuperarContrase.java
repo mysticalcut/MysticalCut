b26d135eef16c3b4470c5930caa7921804707971
Andres1Leon <oaleon06@soy.sena.edu.co>
2025-06-05 19:18:44 -0500

    Se realiza modificaciones en RecuperarContrase en la carpeta de Automatizaciones

--- public/assets/documentacion/DOCUMENTOS_PROYECTO_MYSTICALCUT/Trimestre_5/Automatizaciones/MysticalCut/src/main/java/co/com/Mysticalcut/userinterface/autenticacionRecuperarContrase.java
+++ public/assets/documentacion/DOCUMENTOS_PROYECTO_MYSTICALCUT/Trimestre_5/Automatizaciones/MysticalCut/src/main/java/co/com/Mysticalcut/userinterface/autenticacionRecuperarContrase.java
@@ -6,10 +6,10 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class autenticacionRecuperarContrase extends PageObject{
 
-    public static Target BTN_INICIOSESION = Target.the("Click Boton inicio sesion").located(By.xpath("//[@id=\"app\"]/div[1]/header/div[2]/a[1]"));
-    public static Target BTN_RECUPERARCONTRASE = Target.the("Click Boton inicio sesion").located(By.xpath("//[@id=\"app\"]/div/div/form/div/a[1]"));
-    public static Target INPUT_CORREO = Target.the(" Ingreso del Usuario").located(By.xpath("//[@id=\"app\"]/div/form/input"));
-    public static Target BTN_ENVIAR = Target.the("Click Boton inicio sesion").located(By.xpath("//[@id=\"app\"]/div/form/button"));
+    public static Target BTN_INICIOSESION = Target.the("Click Boton inicio sesion").located(By.xpath("//*[@id=\"app\"]/div[1]/header/div[2]/a[1]"));
+    public static Target BTN_RECUPERARCONTRASE = Target.the("Click Boton inicio sesion").located(By.xpath("//*[@id=\"app\"]/div/div/form/div/a[1]"));
+    public static Target INPUT_CORREO = Target.the(" Ingreso del Usuario").located(By.xpath("//*[@id=\"app\"]/div/form/input"));
+    public static Target BTN_ENVIAR = Target.the("Click Boton inicio sesion").located(By.xpath("//*[@id=\"app\"]/div/form/button"));
 
     public static Target MENSAJE_LOGIN = Target.the("mensaje de login").locatedBy("//button[text()='Enviar']");
 
